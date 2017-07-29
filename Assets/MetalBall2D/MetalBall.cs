2ac0323f43536bc4498810164d9bad350868d815
Chau Giang <giangchau92@gmail.com>
2017-07-29 15:44:20 +0700

    metalball 2D

---
+++ Assets/MetalBall2D/MetalBall.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MetalBall : MonoBehaviour {
+	public int _numberElement = 1;
+	public Material _material;
+	public float _speed = 1.0f;
+	[Range(0, 100)]
+	public float _R = 1.0f;
+	[Range(0, 20)]
+	public float _Gravity = 1.0f;
+
+
+	Vector4[] _elementsPos;
+	Vector4[] _elementVels;
+	Rect _bound;
+
+	// Use this for initialization
+	void Start () {
+		_bound = new Rect (0, 0, Screen.width, Screen.height);
+		_elementsPos = new Vector4[_numberElement];
+		_elementVels = new Vector4[_numberElement];
+		for (int i = 0; i < _numberElement; i++) {
+			_elementsPos [i] = new Vector4 (Screen.width, Screen.height) * Random.value;
+			_elementVels [i] = Random.insideUnitCircle;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < _numberElement; i++) {
+			Vector4 pos = _elementsPos [i];
+			Vector4 vel = _elementVels [i];
+			_elementsPos [i] = pos + vel * Time.deltaTime * _speed;
+			if (_elementsPos [i].x > _bound.xMax || _elementsPos [i].x < _bound.xMin) {
+				_elementVels [i] = new Vector2 (-vel.x, vel.y);
+			}
+			if (_elementsPos [i].y > _bound.yMax || _elementsPos [i].y < _bound.yMin) {
+				_elementVels [i] = new Vector2 (vel.x, -vel.y);
+			}
+
+		}
+	}
+
+	void OnRenderImage(RenderTexture src, RenderTexture dest) {
+		_material.SetFloat ("_R", _R);
+		_material.SetFloat ("_Gravity", _Gravity);
+		_material.SetVectorArray ("_Particles", _elementsPos);
+		_material.SetInt ("_Particles_Length", _numberElement);
+		Graphics.Blit(src, dest, _material);
+	}
+}
---
+++ Assets/MetalBall2D/MetalballUnliShader.shader
@@ -0,0 +1,77 @@
+﻿Shader "Unlit/MetalballUnliShader"
+{
+	Properties
+	{
+		_R("R", float) = 1.0
+		_Gravity("Gravity", float) = 1.0
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				float4 localVertex : COLOR0;
+			};
+
+			uniform float4 _Particles[1000];
+			uniform int _Particles_Length = 10;
+			float _R;
+			float _Gravity;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.localVertex = v.vertex;
+				return o;
+			}
+
+			float3 hsv2rgb(float3 c) {
+              c = float3(c.x, clamp(c.yz, 0.0, 1.0));
+              float4 K = float4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+              float3 p = abs(frac(c.xxx + K.xyz) * 6.0 - K.www);
+              return c.z * lerp(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+            }
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				half sum = 0;
+				for (int it = 0; it < _Particles_Length; it++) {
+					float len = length(i.vertex.xyz - _Particles[it].xyz);
+					float value;
+					if (len < _R) {
+						value = 1;
+					}
+					else {
+						 value = _Gravity / (len - _R);
+					}
+					sum += value;
+				}
+				sum = clamp(sum, 0.3, 1);
+				fixed4 col = float4(hsv2rgb(float3(sum, 1, 1)), 1);
+				return col;
+			}
+
+			
+
+			ENDCG
+		}
+	}
+}
